a7e816cde774b0d1aaa12afe56f9cd4fc5bcd62d
jvens <Jeramie.Vens@gmail.com>
2018-10-07 13:27:38 -0500

    Add Team List page

--- src/views/save/index.vue
+++ src/views/save/index.vue
@@ -1,10 +1,16 @@
 <template>
-  <p>Save</p>
+  <v-card>
+    <v-btn color="success" @click="saveEvent">Save Event</v-btn>
+  </v-card>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    saveEvent() {
+      console.log('save...')
+    }
+  }
 }
 </script>
 
