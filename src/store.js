a715c61e8470798d2cf475a0df73cf7cf97b48bc
jvens <Jeramie.Vens@gmail.com>
2018-10-07 14:29:04 -0500

    add vuex store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,32 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    eventid: "eventid",
+    event: { id: "eventid" }
+  },
+  mutations: {
+    SET_EVENT(state, event) {
+      state.event = event;
+    }
+  },
+  actions: {
+    SAVE_EVENT({ commit, state }, ref) {
+      axios.post('http://localhost:3000/api/event?id=' + state.eventid, state.event)
+      .then(response => {
+      })
+      .catch(e => {
+      })
+    },
+    LOAD_EVENT({ commit, state }, req) {
+      axios.get('http://localhost:3000/api/event?id=' + state.eventid)
+      .then(response => {
+        commit('SET_EVENT', response.data)
+      })
+    }
+  }
 });
--- src/views/save/index.vue
+++ src/views/save/index.vue
@@ -1,14 +1,29 @@
 <template>
   <v-card>
     <v-btn color="success" @click="saveEvent">Save Event</v-btn>
+    <v-btn color="success" @click="loadEvent">Load Event</v-btn>
+    {{response}}
   </v-card>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  data: () => ({
+    response: "response",
+    data: {
+      id: 'eventid'
+    }
+  }),
   methods: {
     saveEvent() {
       console.log('save...')
+      this.$store.dispatch("SAVE_EVENT")
+    },
+    loadEvent() {
+      console.log('load...')
+      this.$store.dispatch("LOAD_EVENT")
     }
   }
 }
